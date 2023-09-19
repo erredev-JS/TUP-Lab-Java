4d674f31bf19baf7fce40bbd10a4ce17bb72f374
Rodrigo Zapata <118647241+erredev-JS@users.noreply.github.com>
2023-09-19 09:23:44 -0300

    repasoParcial

---
+++ repasoParcial.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class repasoParcial {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int n1 = sc.nextInt();
+       int n2 = sc.nextInt();
+       int n1_digit = n1 % 10;
+       int n2_digit = n2 % 10;
+       
+       if(n1_digit == n2_digit){
+         System.out.println("Los ultimos digitos son iguales");
+       }else{
+         System.out.println("Los ultimos digitos no son iguales");
+       }
+}
+}
